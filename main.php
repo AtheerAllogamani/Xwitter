497000f2a0ee0059a035d5db819b1d60e1e7fd0a
Atheer Allogamani <100247724+AtheerAllogamani@users.noreply.github.com>
2023-10-08 01:50:00 +0300

    Update main.php

--- main.php
+++ main.php
@@ -12,18 +12,25 @@ if (isset($_SESSION["user_id"])) {
     $result = $mysqli->query($sql);
     
     $user = $result->fetch_assoc();
-}
 
-if (isset($_POST["post"])) {
-    $status = 'error';
-    $user_input = $_POST["user_input"];
-    $updated = $mysqli->query("UPDATE user SET post='$user_input' WHERE id = {$_SESSION["user_id"]}");
-
-    if ($updated) {
-        $status = 'success';
-    } else {
+    if (isset($_POST["submit"])) {
+        $status = '';
+        $user_input = $_POST["user_input"]; 
+        if (empty($user_input)) {
+            $status = "You have to write something";
+        } else {
+            $user_id = $_SESSION["user_id"];
+            $updated = $mysqli->query("INSERT INTO posts (content, user_id) VALUES ('$user_input', {$_SESSION["user_id"]})");
+            header("Location: main.php");
+            exit;
+        }
     }
+    
+    $posts = $mysqli->query("SELECT * FROM posts ORDER BY post_id DESC");
+    
 }
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -33,9 +40,16 @@ if (isset($_POST["post"])) {
 </head>
 <body>
 <style>
+        .container {
+        display: flex;
+        justify-content: space-around;
+        text-align: center;
+        background-color: #05314c;
+        margin: 0 auto;
+        }
+
         .upper{
             margin-top: 50px;
-            margin-right: 3%;
         }
 
         input[type=text] {
@@ -55,37 +69,39 @@ if (isset($_POST["post"])) {
         }
 
         #output {
+        display: inline-flexbox;
         color: aliceblue;
         list-style-type: none;
-        flex-direction: column-reverse;
-        margin: 5px;
         background-color: #232323;
         padding: 15px;
         border: 2px solid aliceblue;
         border-radius: 15px;
         width: 70%;
-        margin-left: 25%;
         }
+
         #output:before {
             content: "";
-            display: inline-block;
+            display: inline-flex;
             background-size: cover;
             margin-right: 10px;
-            background-image: url('<?php echo isset($user['profile_pic']) ? 'data:image/jpeg;base64,' . base64_encode($user['profile_pic']) : '/img/default-profile-pic.jpg'; ?>');
+            background-image: url('<?php echo isset($user['profile_pic']) && !empty($user['profile_pic']) ? 'data:image/jpeg;base64,' . base64_encode($user['profile_pic']) : '/img/default-profile-pic.png'; ?>');
             border: 2px solid aliceblue;
             border-radius: 50%;
             width: 50px;
             height: 50px;
             
         }
-        #output:after {
+        .name {
             position: absolute;
             font-weight: bold;
-            content: "<?php echo htmlspecialchars($user['name']); ?>";
+            content: "";
         }
 
         .profileContiner{
+            display: inline-flexbox;
+            flex-direction: row;  
             margin-left: 3%;
+            margin-right: 3%;
             padding: 2%;
             width: 15%;
             background-color: aliceblue;
@@ -93,33 +109,49 @@ if (isset($_POST["post"])) {
             border: 1px solid #05314c;
             border-radius: 25px;
         }
+
+        .outer-container{
+        display: flex;  
+        flex-direction: row;    
+        }
+        .inner-container{
+            display: flex;  
+            flex-direction: column;   
+            width: 70%;
+        }
     </style>
 
     <div class="container">
-    <a href="main.php"><img src="img/Xwitter.png"></a>
-        <div class="upper">
-            <?php if (isset($user)): ?>
-                <p><a href="logout.php">Log out</a></p>
-            <?php else: ?>
-                <p><a href="login.php">Log in</a> or <a href="signup.html">sign up</a></p>
-            <?php endif; ?>
-        </div>
+        <a href="main.php"><img src="img/Xwitter.png"></a>
+            <div class="upper">
+                <?php if (isset($user)): ?>
+                    <p><a href="logout.php">Log out</a></p>
+                <?php else: ?>
+                    <p><a href="login.php">Log in</a> or <a href="signup.html">sign up</a></p>
+                <?php endif; ?>
+            </div>
     </div>
 
     <form action="" method="POST" enctype="multipart/form-data">
         <input type="text" name="user_input" placeholder="write your thoughts">
-        <input type="submit" onclick="displayInput()" name="post" value="POST">
+        <input type="submit" name="submit" value="POST">
     </form>
-
-    <p id="output"><?= htmlspecialchars($user["post"]) ?></p>
-
-    <div class="profileContiner">
-        <img src="<?php echo isset($user['profile_pic']) ? 'data:image/jpeg;base64,' . base64_encode($user['profile_pic']) : '/img/icon.jpg'; ?>" id="profilePic">
-        <p><strong>Name: </strong><?= htmlspecialchars($user["name"]) ?></p>
-        <p><strong>Email: </strong><?= htmlspecialchars($user["email"]) ?></p>
-        <a href="edit_profile.php" class="button" style="text-decoration: underline; color:#05314c;"><strong>Edit Profile</strong></a>
+    <div class="outer-container">
+        <div class="profileContiner">
+            <img src="<?php echo isset($user['profile_pic']) ? 'data:image/jpeg;base64,' . base64_encode($user['profile_pic']) : '/img/icon.jpg'; ?>" id="profilePic">
+            <p><strong>Name: </strong><?= htmlspecialchars($user["name"]) ?></p>
+            <p><strong>Email: </strong><?= htmlspecialchars($user["email"]) ?></p>
+            <a href="edit_profile.php" class="button" style="text-decoration: underline; color:#05314c;"><strong>Edit Profile</strong></a>
+        </div>
+        <div class="inner-container">
+            <?php while ($row = $posts->fetch_assoc()) { ?>
+                <p id="output">
+                    <span class="name"><?php echo htmlspecialchars($user['name']); ?></span>
+                    <?php echo htmlspecialchars($row["content"]); ?>
+                </p>
+            <?php } ?>
+        </div>
     </div>
-
 </body>
 <script src="script.js"></script>
 </html>
